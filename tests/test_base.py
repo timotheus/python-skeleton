9794bfca06a2c60fe7c7ccf6bcd56ea714c56f7d
Tim Keefer <tkeefer@gmail.com>
2017-03-27 22:30:46 -0700

    add logging examples

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,13 +1,19 @@
 import os
 import unittest
+import logging
 
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
 
 class TestBase(unittest.TestCase):
 
     def test_command(self):
-        # base_dir = os.path.dirname(os.path.abspath(__file__))
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        logger.info(f"base directory: {base_dir}")
         self.assertEqual('asdf', 'asdf')
 
 
 if __name__ == '__main__':
+    logger.info('calling unittest.main()')
     unittest.main()
+    logger.info('end unittest.main()')
