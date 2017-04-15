25c92c9a8af62e6847f9aaf93daa93cb4822c87c
Tim Keefer <tkeefer@gmail.com>
2017-04-15 10:39:35 -0700

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend'
+    'django.contrib.auth.backends.ModelBackend',
 )
 
 # Internationalization
